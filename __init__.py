7769397b4732f9e5ae17bbf1f6286f953df3bf21
anas-dew <mailtoanas@duck.com>
2022-06-09 22:18:27 +0530

    password feature added

--- __init__.py
+++ __init__.py
@@ -1,28 +1,30 @@
 from flask import Flask, render_template, redirect, request
 from utilities import isURLValid, randomLinkCode
 # from db_mongo import pushToDatabase, searchInDatabase, getLink
-from db_sql import pushToDatabase, searchInDatabase, getLink
+from db_sql import checkPass, matchPassword, pushToDatabase, searchInDatabase, getLink
 import re
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def runHome():
     if request.method == "POST":
         link_code = randomLinkCode()
         original_link = request.form.get("og-link")
+        passcode = request.form.get("password-here")
 
         if len(original_link) <= 0:
             return render_template('home.html', push_link='Put a link !!', abbr="Haha still you didn't generated any link, get one buddy!")
-        
+
         elif isURLValid(original_link) == False:
             return render_template('home.html', push_link='Invalid URL', abbr="Haha still you didn't generated any link, get one buddy!")
-        
+
         if searchInDatabase(link_code) == True:
             link_code = randomLinkCode()
 
         if re.search("^https://", original_link) or re.search("^http://", original_link):
-            pushToDatabase(link_code, original_link)
+            pushToDatabase(link_code, original_link, passcode)
         else:
             pushToDatabase(link_code, f"https://{original_link}")
 
@@ -33,16 +35,26 @@ def runHome():
     return render_template('home.html', push_link='Generate a link !', abbr="Haha you didn't generated any link, get one buddy!")
 
 
-@app.route('/<link_code>')
+@app.route('/<link_code>', methods=['GET', 'POST'])
 def redirectToOriginalLink(link_code):
     if searchInDatabase(link_code) == True:
-        return redirect(getLink(link_code))
+        if checkPass(link_code) == True:
+            if request.method == "POST":
+                passcode = request.form.get("passcode")
+                if matchPassword(link_code, passcode) == True:
+                    return redirect(getLink(link_code))
+                return render_template('auth_redirect.html', input_message="Incorrect password !!!")
+            return render_template('auth_redirect.html', input_message="Enter secret password")
+        else:
+            return redirect(getLink(link_code))
     else:
         return render_template('not_found.html')
 
-@app.route('/report-a-problem')
+
+@app.route('/report-a-problem', methods=['GET', 'POST'])
 def redirectToReportPage():
     return render_template('report_a_problem.html')
-    
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- db_sql.py
+++ db_sql.py
@@ -4,19 +4,29 @@ db = quicksqlconnector('localhost', 6606,'root', 'anas9916', 'superurl')
 
 db.query('create database if not exists superurl')
 db.query('use superurl')
-db.query('create table if not exists linkBase(id varchar(10) primary key, originalLink varchar(1000))')
+db.query('create table if not exists linkBase(id varchar(7) primary key, passCode varchar(50), originalLink varchar(2000))')
 
-def pushToDatabase(link_code: str, original_link: str):
-    db.query(f'insert into linkBase values("{link_code}","{original_link}")')
+def pushToDatabase(link_code: str, original_link: str, passcode : str):
+    db.query(f'insert into linkBase values("{link_code}","{passcode}","{original_link}")')
 
 def searchInDatabase(link_code: str):
     url = db.query(f'select originalLink from linkBase where id="{link_code}"')
     if url : return True
-    else :return False
-
+    else : return False
 
 def getLink(link_code: str):
     return db.query(f'select originalLink from linkBase where id="{link_code}"')[0][0]
+
+def checkPass(link_code: str):
+    password = db.query(f'select passCode from linkBase where id="{link_code}"')[0][0]
+    if password : return True
+    else : return False
+
+def matchPassword(link_code: str, passcode : str):
+    password = db.query(f'select passCode from linkBase where id="{link_code}"')[0][0]
+    if passcode == password:
+        return True
+    return False
 
 if __name__ == "__main__":
     pass
