e78bd7dae02684d0f861c64a4664c711051f423b
anas-dew <mailtoanas@duck.com>
2022-12-30 10:30:53 +0530

    newsite map and f-string removed

--- db_sql.py
+++ db_sql.py
@@ -8,35 +8,35 @@ db.query('create table if not exists linkBase(id varchar(100) primary key, passC
 db.query('create table if not exists problemReported(msg varchar(500) primary key)')
 
 def pushToDatabase(link_code: str, passcode : str, original_link: str):
-    db.query(f'insert into linkBase values(%s, %s, %s)', (link_code, passcode, original_link))
+    db.query('insert into linkBase values(%s, %s, %s)', (link_code, passcode, original_link))
 
 def searchInDatabase(link_code: str):
-    url = db.query(f'select originalLink from linkBase where id=%s', (link_code,))
+    url = db.query('select originalLink from linkBase where id=%s', (link_code,))
     if url : return True
     else : return False
 
 def getLink(link_code: str):
-    return db.query(f'select originalLink from linkBase where id=%s', (link_code,))[0][0]
+    return db.query('select originalLink from linkBase where id=%s', (link_code,))[0][0]
 
 def checkPass(link_code: str):
-    password = db.query(f'select passCode from linkBase where id=%s', (link_code,))[0][0]
+    password = db.query('select passCode from linkBase where id=%s', (link_code,))[0][0]
     if password : return True
     else : return False
 
 def matchPassword(link_code: str, passcode : str):
-    password = db.query(f'select passCode from linkBase where id=%s', (link_code,))[0][0]
+    password = db.query('select passCode from linkBase where id=%s', (link_code,))[0][0]
     if passcode == password:
         return True
     return False
 
 def availableHandle(custom_handle : str):
     print(custom_handle)
-    custom_handle_found = db.query(f'select id from linkBase where id=%s', (custom_handle,))
+    custom_handle_found = db.query('select id from linkBase where id=%s', (custom_handle,))
     if custom_handle_found : return True
     else : return False
 # -----------------------------------
 def submitProblem(problem_statement : str):
-    db.query(f'insert into problemReported values(%s)', (problem_statement,))
+    db.query('insert into problemReported values(%s)', (problem_statement,))
 
 if __name__ == "__main__":
     # print(availableHandle('mylovelysite'))
